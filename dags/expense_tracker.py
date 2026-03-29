379b3ade3686bf594cb8c04b4267bbe276672a7f
lohiavardhan <lohiavardhan@gmail.com>
2026-03-29 21:23:47 +0800

    added the logic to seperate the emails from DBS and also log all other emails

--- dags/expense_tracker.py
+++ dags/expense_tracker.py
@@ -1,5 +1,5 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import pickle
 import os.path
@@ -7,6 +7,7 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import json
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
@@ -19,18 +20,18 @@ default_args = {
 def get_gmail_service():
     creds = None
     # token.json stores the user's access/refresh tokens after first auth
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('../token.json'):
+        creds = Credentials.from_authorized_user_file('../token.json', SCOPES)
 
     # If no valid creds, do the OAuth flow
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('../credentials.json', SCOPES)
             creds = flow.run_local_server(port=8080)
 
-        with open('token.json', 'w') as token:
+        with open('../token.json', 'w') as token:
             token.write(creds.to_json())
 
     return build('gmail', 'v1', credentials=creds)
@@ -39,10 +40,21 @@ def fetch_emails():
     """Pull all emails from Gmail API"""
 
     service = get_gmail_service()
-    results = service.users().messages().list(userId='me', q='newer_than:1h').execute()
+    results = service.users().messages().list(userId='me', q='newer_than:6h').execute()
     messages = results.get('messages', [])
+    banking_transactions = []
+    all_transactions = []
+
+    for i, msg in enumerate(messages):
+        detail = service.users().messages().get(userId='me', id=msg['id']).execute()
+        for payload_dict in detail['payload']['headers']:
+            if payload_dict['name'] == "From":
+                if payload_dict['value'] == "\"ibanking.alert@dbs.com\" <ibanking.alert@dbs.com>":
+                    banking_transactions.append(detail)
+        
+        all_transactions.append(detail)
     
-    print("Fetching emails...")
+    return banking_transactions, all_transactions
     
 
 def parse_transactions(**context):
@@ -60,19 +72,21 @@ def load_to_db(**context):
     # TODO: Database insert
     print("Loading to database...")
 
-with DAG(
-    dag_id='expense_tracker',
-    default_args=default_args,
-    description='Track expenses from bank emails',
-    schedule_interval='@hourly',
-    start_date=datetime(2026, 3, 29),
-    catchup=False,
-    tags=['expenses'],
-) as dag:
+fetch_emails()
 
-    t1 = PythonOperator(task_id='fetch_emails', python_callable=fetch_emails)
-    t2 = PythonOperator(task_id='parse_transactions', python_callable=parse_transactions)
-    t3 = PythonOperator(task_id='categorize', python_callable=categorize)
-    t4 = PythonOperator(task_id='load_to_db', python_callable=load_to_db)
+# with DAG(
+#     dag_id='expense_tracker',
+#     default_args=default_args,
+#     description='Track expenses from bank emails',
+#     schedule_interval='@hourly',
+#     start_date=datetime(2026, 3, 29),
+#     catchup=False,
+#     tags=['expenses'],
+# ) as dag:
 
-    t1 >> t2 >> t3 >> t4
+#     t1 = PythonOperator(task_id='fetch_emails', python_callable=fetch_emails)
+#     t2 = PythonOperator(task_id='parse_transactions', python_callable=parse_transactions)
+#     t3 = PythonOperator(task_id='categorize', python_callable=categorize)
+#     t4 = PythonOperator(task_id='load_to_db', python_callable=load_to_db)
+
+#     t1 >> t2 >> t3 >> t4
